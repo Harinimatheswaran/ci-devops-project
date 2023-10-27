c2062ff7c99765d66b9fb88d20f404fe5e8ff7b5
ProDesignerNurul <maxnewsbd@gmail.com>
2023-10-27 12:56:55 +0600

    complete ema-john nav bar with logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className='logo-img'>
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className='nav-menu'>
+                <a href="/home">Home</a>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
